9e672f1e19af71143981e8e30a173452cabfe35e
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-01 13:18:24 +0530

    encryption of password

--- routes/User.js
+++ routes/User.js
@@ -1,19 +1,27 @@
 var User = require("../models/User");
 var express = require("express");
 var route = express.Router();
+var bcrypt = require("bcrypt");
+var saltRounds = 10;
 
 route.post("/signup", async (req, res) => {
   var NewUser = new User({
     name: req.body.name,
     password: req.body.password
   });
-  await NewUser.save()
-    .then(person => {
-      res.send(person);
-    })
-    .catch(err => {
-      console.log("Error is ", err.message);
+
+  await bcrypt.genSalt(saltRounds, async (err, salt) => {
+    await bcrypt.hash(NewUser.password, salt, async (err, hash) => {
+      NewUser.password = hash;
+      await NewUser.save()
+        .then(person => {
+          res.send(person);
+        })
+        .catch(err => {
+          console.log("Error is ", err.message);
+        });
     });
+  });
 });
 
 route.post("/login", async (req, res) => {
@@ -23,11 +31,15 @@ route.post("/login", async (req, res) => {
 
   await User.findOne({ name: ExistingUser.name })
     .then(person => {
-      if (ExistingUser.password === person.password) {
-        res.send(`User is logged in successfully...`);
-      } else {
-        res.send(`Unauthorized Access:Wrong Password`);
-      }
+      bcrypt.compare(ExistingUser.password, person.password, (err, result) => {
+        if (result) {
+          res.send(`User is logged in successfully...`);
+        } else if (err) {
+          console.log("Error is ", err.message);
+        } else {
+          res.send(`Unauthorized Access:Wrong Password`);
+        }
+      });
     })
     .catch(err => {
       console.log("Error is ", err.message);
