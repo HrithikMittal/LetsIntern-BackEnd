fbb808a21d17cc40f0a2d309a1c319c9c1aa5407
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-01 13:30:27 +0530

    Unique Username

--- routes/User.js
+++ routes/User.js
@@ -9,19 +9,28 @@ route.post("/signup", async (req, res) => {
     name: req.body.name,
     password: req.body.password
   });
-
-  await bcrypt.genSalt(saltRounds, async (err, salt) => {
-    await bcrypt.hash(NewUser.password, salt, async (err, hash) => {
-      NewUser.password = hash;
-      await NewUser.save()
-        .then(person => {
-          res.send(person);
-        })
-        .catch(err => {
-          console.log("Error is ", err.message);
+  User.findOne({ name: NewUser.name })
+    .then(async person => {
+      if (!person) {
+        await bcrypt.genSalt(saltRounds, async (err, salt) => {
+          await bcrypt.hash(NewUser.password, salt, async (err, hash) => {
+            NewUser.password = hash;
+            await NewUser.save()
+              .then(person => {
+                res.send(person);
+              })
+              .catch(err => {
+                console.log("Error is ", err.message);
+              });
+          });
         });
+      } else {
+        res.send(`this username already exists`);
+      }
+    })
+    .catch(err => {
+      console.log("Error is ", err.message);
     });
-  });
 });
 
 route.post("/login", async (req, res) => {
@@ -30,16 +39,20 @@ route.post("/login", async (req, res) => {
   ExistingUser.password = req.body.password;
 
   await User.findOne({ name: ExistingUser.name })
-    .then(person => {
-      bcrypt.compare(ExistingUser.password, person.password, (err, result) => {
-        if (result) {
-          res.send(`User is logged in successfully...`);
-        } else if (err) {
-          console.log("Error is ", err.message);
-        } else {
-          res.send(`Unauthorized Access:Wrong Password`);
+    .then(async person => {
+      await bcrypt.compare(
+        ExistingUser.password,
+        person.password,
+        (err, result) => {
+          if (result) {
+            res.send(`User is logged in successfully...`);
+          } else if (err) {
+            console.log("Error is ", err.message);
+          } else {
+            res.send(`Unauthorized Access:Wrong Password`);
+          }
         }
-      });
+      );
     })
     .catch(err => {
       console.log("Error is ", err.message);
