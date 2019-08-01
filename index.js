563e401478a67dbd09b9d862601b6e4712429a31
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-01 12:53:30 +0530

    signup route for the user

--- index.js
+++ index.js
@@ -22,15 +22,32 @@ app.get("/", (req, res) => {
   res.send(`Hi I am Adhikansh Mittal`);
 });
 
-app.post("/user/signup", (req, res) => {
+app.post("/user/signup", async (req, res) => {
   var NewUser = new User({
     name: req.body.name,
     password: req.body.password
   });
-  console.log(NewUser.name);
-  NewUser.save()
+  await NewUser.save()
     .then(person => {
       res.send(person);
+    })
+    .catch(err => {
+      console.log("Error is ", err.message);
+    });
+});
+
+app.post("/user/login", async (req, res) => {
+  var ExistingUser = {};
+  ExistingUser.name = req.body.name;
+  ExistingUser.password = req.body.password;
+
+  await User.findOne({ name: ExistingUser.name })
+    .then(person => {
+      if (ExistingUser.password === person.password) {
+        res.send(`User is logged in successfully...`);
+      } else {
+        res.send(`Unauthorized Access:Wrong Password`);
+      }
     })
     .catch(err => {
       console.log("Error is ", err.message);
