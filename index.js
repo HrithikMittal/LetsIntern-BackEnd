0be7a430b127c522bf91c84f387b2670f46889f1
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-01 12:26:28 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 var mongoose = require("mongoose");
 var express = require("express");
+var bodyparser = require("body-parser");
 var app = express();
+var User = require("./models/User");
+
+mongoose
+  .connect("mongodb://localhost:27017/quiz")
+  .then(() => {
+    console.log("Database is connected successfully...");
+  })
+  .catch(err => {
+    console.log("Error is ", err.message);
+  });
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send(`Hi I am Adhikansh Mittal`);
+});
+
+app.post("/user/signup", (req, res) => {
+  var NewUser = User({
+    name: req.body.name,
+    password: req.body.password
+  });
+  NewUser.save();
 });
 
 app.listen(port, () => {
