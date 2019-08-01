d18980bf7b81262fd1f7a6dc467a3495e275402a
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-08-01 22:58:06 +0530

    update the test model

--- models/Test.js
+++ models/Test.js
@@ -1,6 +1,21 @@
 var mongoose = require('mongoose');
 var TestSchema = mongoose.Schema({
-    
+    test_name:{
+        type:String,
+        require:true
+    },
+    max_score:{
+        type:String,
+        require:true
+    },
+    question:[
+        {
+          question:{  
+              type:Schema.Types.ObjectId,
+              ref:"Question"
+          }
+        }
+    ]
 });
 
 module.exports = Test = mongoose.model('Test',TestSchema);
