7e275b5f8f54c0937415445d3d436f40c98d408f
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-02 16:14:46 +0530

    apply for internship

--- routes/Internship.js
+++ routes/Internship.js
@@ -30,4 +30,48 @@ router.post("/internship-post", async (req, res) => {
     });
 });
 
+router.get("/internship-get", async (req, res) => {
+  await Internship.find()
+    .then(intern => {
+      if (intern) {
+        res.send(intern);
+      } else {
+        res.send(`There is no internship`);
+      }
+    })
+    .catch(err => {
+      console.log("Error is ", err.message);
+    });
+});
+
+router.post("/internship-apply", async (req, res) => {
+  var internshipId = req.body.internshipId;
+  var studentId = req.body.studentId;
+  var student = {};
+  student.studentId = studentId;
+  await Internship.findOne({ _id: internshipId })
+    .then(async intern => {
+      if (intern) {
+        intern["students"].push(student);
+        await intern
+          .save()
+          .then(value => {
+            if (value) {
+              res.send(value);
+            } else {
+              res.send("Try Again There is some problem");
+            }
+          })
+          .catch(err => {
+            console.log("Error is ", err.message);
+          });
+      } else {
+        res.send("this Internship doesn't exists");
+      }
+    })
+    .catch(err => {
+      console.log("Error is ", err.message);
+    });
+});
+
 module.exports = router;
